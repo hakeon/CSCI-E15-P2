483e5fbbf2dca9ce3f63ddac3986779a46ec1437
HAKEON <hakeon@gmail.com>
2014-10-01 23:05:02 -0400

    added link to pure css to index head

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(-1); # Report all PHP errors
-ini_set("display_errors", 1);
+ini_set('display_errors', 1);
 ?>
 
 <!DOCTYPE html>
@@ -11,14 +11,17 @@ ini_set("display_errors", 1);
     <title>CSCI E-15 P2</title>
     <meta charset='utf-8'>
 
-    <link rel='stylesheet' href='style.css' type='text.css>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <?php require 'logic.php'; ?>
 
 </head>
 
 <body>
-
+    <h1> "Hello, World!" </h1>
     <h1> <?php echo $time; ?> </h1>
 
 </body>
